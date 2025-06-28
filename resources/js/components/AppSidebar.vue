7d40495eb6c28c13f2f3fe616045670e3bba2d96
Akash <akashmitra@gmail.com>
2025-06-28 11:57:53 +0800

    sidebar icon update

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -5,7 +5,7 @@ import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import { BookOpen, Folder, LayoutGrid, Music4, Guitar } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
@@ -17,12 +17,12 @@ const mainNavItems: NavItem[] = [
     {
         title: 'Themes',
         href: '/themes',
-        icon: LayoutGrid,
+        icon: Music4,
     },
     {
         title: 'Courses',
         href: '/courses',
-        icon: LayoutGrid,
+        icon: Guitar,
     },
 ];
 
